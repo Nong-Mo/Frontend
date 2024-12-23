50a32c9e861268b5e1dae55545bcc981f0625865
hanvy072 <072hanvy@gmail.com>
2024-12-24 05:28:17 +0900

    [Fix] Add MoveScanCard to LibraryScreen
    내 서재 화면에 MoveScanCard 보이도록 추가

--- src/screens/library/LibraryScreen.jsx
+++ src/screens/library/LibraryScreen.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import { View, ScrollView } from 'react-native';
 import MainHeader from '../../components/navigations/header/MainHeader';
+import MoveScanCard from '../../components/shared/MoveScanCard'; // MoveScanCard import
 
 const LibraryScreen = () => {
   return (
     <View className="flex-1 bg-white">
       <MainHeader />
-      <ScrollView>{/* 콘텐츠 영역 */}</ScrollView>
+      <ScrollView contentContainerStyle={{ flexGrow: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <MoveScanCard /> {/* MoveScanCard 컴포넌트 추가 */}
+      </ScrollView>
     </View>
   );
 };
