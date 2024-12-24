e888a09c35718ab208d3b0a5c141a25e9088a52c
rbase0218 <rbase@naver.com>
2024-12-24 14:11:20 +0900

    [Fix] Player Screen의 Bottom Nav에서 필요 없는 아이콘 제거

--- src/components/navigations/bottom-bar/PlayerBottomBar.jsx
+++ src/components/navigations/bottom-bar/PlayerBottomBar.jsx
@@ -1,14 +1,11 @@
 import React from 'react';
-import { View, TouchableOpacity } from 'react-native';
+import { View, TouchableOpacity, StyleSheet } from 'react-native';
 import Icon from 'react-native-vector-icons/MaterialIcons';
 
 const PlayerBottomBar = () => {
   return (
-    <View className="h-20 bg-white px-4">
+    <View className="h-20 bg-white px-4" style={styles.playerBotomBarContainer}>
       <View className="flex-row justify-between items-center h-full">
-        <TouchableOpacity>
-          <Icon name="skip-previous" size={32} />
-        </TouchableOpacity>
         <TouchableOpacity>
           <Icon name="replay-10" size={32} />
         </TouchableOpacity>
@@ -18,12 +15,18 @@ const PlayerBottomBar = () => {
         <TouchableOpacity>
           <Icon name="forward-10" size={32} />
         </TouchableOpacity>
-        <TouchableOpacity>
-          <Icon name="skip-next" size={32} />
-        </TouchableOpacity>
       </View>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  playerBotomBarContainer: {
+    width: '65%',
+    borderColor: 'black',
+    borderRadius: 10,
+    borderWidth: 1,
+  },
+});
 
 export default PlayerBottomBar;
