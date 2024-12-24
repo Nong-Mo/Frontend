685309a3077a5989cc7ef3749b8941a3c9dcc755
rbase0218 <rbase@naver.com>
2024-12-24 14:12:04 +0900

    [Fix] 불필요한 SelectImage Props 삭제

--- src/components/player/ImageViewer.jsx
+++ src/components/player/ImageViewer.jsx
@@ -1,9 +1,8 @@
 import { StyleSheet } from 'react-native';
 import { Image } from 'expo-image';
 
-export default function ImageViewer({ imgSource, selectedImage }) {
-  const imageSource = selectedImage ? { uri: selectedImage } : imgSource;
-  return <Image source={imageSource} style={styles.image} />;
+export default function ImageViewer({ imgSource }) {
+  return <Image source={imgSource} style={styles.image} />;
 }
 
 const styles = StyleSheet.create({
