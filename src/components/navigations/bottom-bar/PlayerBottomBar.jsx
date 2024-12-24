0a0becdcc74354c73502ec1db0423b74b586efa0
rbase0218 <rbase@naver.com>
2024-12-24 16:36:42 +0900

    [Fix] Audio Player Screen 가배치

--- src/components/navigations/bottom-bar/PlayerBottomBar.jsx
+++ src/components/navigations/bottom-bar/PlayerBottomBar.jsx
@@ -1,11 +1,27 @@
 import React from 'react';
-import { View, TouchableOpacity, StyleSheet } from 'react-native';
+import { Text, View, TouchableOpacity, StyleSheet } from 'react-native';
+import { Slider } from 'react-native-awesome-slider';
 import Icon from 'react-native-vector-icons/MaterialIcons';
+import { useSharedValue } from 'react-native-reanimated';
 
 const PlayerBottomBar = () => {
+  const progress = useSharedValue(0.35);
+  const min = useSharedValue(0);
+  const max = useSharedValue(100);
+
   return (
-    <View className="h-20 bg-white px-4" style={styles.playerBotomBarContainer}>
-      <View className="flex-row justify-between items-center h-full">
+    <View className="h-20 bg-white px-4" style={styles.audioPlayerContainer}>
+      <View style={styles.audioInfoContainer}>
+        <Slider progress={progress} minimumValue={min} maximumValue={max} />
+        <View className="flex-row justify-between">
+          <Text style={styles.timeText}>00:00</Text>
+          <Text style={styles.timeText}>10:00</Text>
+        </View>
+      </View>
+      <View
+        className="flex-row items-center h-full"
+        style={styles.buttonContainer}
+      >
         <TouchableOpacity>
           <Icon name="replay-10" size={32} />
         </TouchableOpacity>
@@ -21,11 +37,25 @@ const PlayerBottomBar = () => {
 };
 
 const styles = StyleSheet.create({
-  playerBotomBarContainer: {
-    width: '65%',
+  audioPlayerContainer: {
+    width: '100%',
     borderColor: 'black',
     borderRadius: 10,
     borderWidth: 1,
+  },
+  audioInfoContainer: {
+    height: '50%',
+    borderColor: 'black',
+    borderRadius: 10,
+    borderWidth: 1,
+  },
+  buttonContainer: {
+    justifyContent: 'space-around',
+  },
+  timeText: {
+    opacity: 0.75,
+    color: 'gray',
+    fontWeight: 'bold',
   },
 });
 
