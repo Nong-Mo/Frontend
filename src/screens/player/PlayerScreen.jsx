b6eab045e809eac40856b0c69945bd239c35a91e
rbase0218 <rbase@naver.com>
2024-12-24 13:09:32 +0900

    [Fix] ImageViewer Position 중앙 배치

--- src/screens/player/PlayerScreen.jsx
+++ src/screens/player/PlayerScreen.jsx
@@ -1,16 +1,33 @@
 import React from 'react';
-import { View } from 'react-native';
+import { View, StyleSheet } from 'react-native';
 import PlayerHeader from '../../components/navigations/header/PlayerHeader';
 import PlayerBottomBar from '../../components/navigations/bottom-bar/PlayerBottomBar';
+import ImageViewer from '../../components/player/ImageViewer';
+
+const PlaceholderImage = require('../../../assets/icon.png');
 
 const PlayerScreen = () => {
   return (
-    <View className="flex-1 bg-white">
+    <View style={styles.container}>
       <PlayerHeader />
-      {/* 플레이어 영역 */}
+      <View style={styles.ImageViewerContainer}>
+        <ImageViewer imgSource={PlaceholderImage} />
+      </View>
       <PlayerBottomBar />
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'white',
+  },
+  ImageViewerContainer: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
 
 export default PlayerScreen;
