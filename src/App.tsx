d7128aa31359b054ea32aeae1a587fc13d7bb015
rbase0218 <rbase@naver.com>
2024-12-28 22:02:42 +0900

    [Chore] React Three Fiber 라이브러리 추가

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,7 @@ import React from 'react';
 import './App.css';
 
 function App() {
-  return (
-      <div className="Nongmo">
-        <h1 className="text-3xl font-bold underline">
-          Hello world!
-        </h1>
-      </div>
-  );
+  return;
 }
 
 export default App;
