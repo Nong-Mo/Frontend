c441e10a486f910e634104a66ee5e055014d0105
rbase0218 <rbase@naver.com>
2024-12-28 22:25:58 +0900

    [Refactor] 파일 문법 수정 및 Prettier 설정

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,12 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import './index.css';
-import App from './App';
-import * as serviceWorkerRegistration from './serviceWorkerRegistration';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
+import reportWebVitals from "./reportWebVitals";
 
-const root = createRoot(document.getElementById('root') as HTMLElement);
-root.render(
-    <App />
-);
+const root = createRoot(document.getElementById("root") as HTMLElement);
+root.render(<App />);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
