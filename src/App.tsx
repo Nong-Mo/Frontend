76fe8922bbeee745808981fc1c8887d7c90cb694
rbase0218 <rbase@naver.com>
2024-12-29 22:55:00 +0900

    [Fix] Router에 Scan Page 연동하기

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,7 @@ import {
 import Login from "./pages/Login";
 import SignIn from "./pages/SignIn";
 import SignUp from "./pages/SignUp";
+import Scan from "./pages/Scan";
 import Sidebar from "./components/common/Sidebar";
 
 // CSS
@@ -19,8 +20,9 @@ const App = () => {
       <Router>
         <main className="content-container flex justify-center bg-red-400">
           <Routes>
-            <Route path="/" element={<Navigate to="/signin" replace />} />
+            <Route path="/" element={<Navigate to="/scan" replace />} />
             <Route path="/signin" element={<SignIn />} />
+            <Route path="/scan" element={<Scan />} />
             <Route path="/signup" element={<SignUp />} />
             <Route path="*" element={<Navigate to="/signin" replace />} />
           </Routes>
---
+++ src/pages/Scan.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Scan = () => {
+  return (
+    <div>
+      <h1>Hi Scan Page</h1>
+    </div>
+  );
+};
+
+export default Scan;
