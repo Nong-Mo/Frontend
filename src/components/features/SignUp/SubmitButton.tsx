8f1d95d2687d775684eb2c6d5f71acfa5bc9e470
hanvy072 <072hanvy@gmail.com>
2024-12-29 21:05:22 +0900

    [Fix] 회원가입 버튼 컴포넌트 생성

---
+++ src/components/features/SignUp/SubmitButton.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+interface SubmitButtonProps {
+    onClick?: () => void,
+    children: React.ReactNode,
+    className: string
+}
+
+const SubmitButton: React.FC<SubmitButtonProps> = ({onClick, children, className}) => {
+    return (
+        <button
+            onClick={onClick}
+            className="bg-blue-500 text-white p-2 rounded w-full hover:bg-blue-600 focus:outline-none focus:ring focus:ring-blue-300"
+        >
+            {children}
+        </button>
+    );
+};
+
+export default SubmitButton;
