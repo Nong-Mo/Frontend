2b127bec2e3de21a13bd08ff2e70e23a62069971
hanvy072 <072hanvy@gmail.com>
2024-12-30 21:01:18 +0900

    [Chore] SignIn와 SignUp에 사용되는 컴포넌트 통합을 위해 컴포넌트 폴더명 변경

---
+++ src/components/features/Sign/InputField.tsx
@@ -0,0 +1,21 @@
+const InputField = ({ label, type, name, value, onChange, placeholder, error }) => {
+    return (
+        <div className="mb-4">
+            <label className="block text-sm font-medium mb-1">{label}</label>
+            <input
+                type={type}
+                name={name}
+                value={value}
+                onChange={onChange}
+                placeholder={placeholder}
+                className={`w-full border p-2 rounded-full ${
+                    error ? 'border-red-500' : 'border-gray-300'
+                }`}
+                required
+            />
+            {error && <p className="text-red-500 text-xs mt-1">{error}</p>}
+        </div>
+    );
+};
+
+export default InputField;
