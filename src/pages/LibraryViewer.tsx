fb16acb1cf5f7c7f1295809c2a79ee98ec31eee9
rbase0218 <rbase@naver.com>
2024-12-31 19:19:37 +0900

    [Feat] LibraryViewer 페이지 컨테이너 작업

---
+++ src/pages/LibraryViewer.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+import { NavBar } from "../components/common/NavBar";
+
+const LibraryViewer = () => {
+  return (
+    <div className="page-container">
+      <NavBar />
+      <h1>Welcome to Library Viewer</h1>
+    </div>
+  );
+};
+
+export default LibraryViewer;
