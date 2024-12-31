86f4ce70727c1afc25bb7343918dbdd2617a3dae
hanvy072 <072hanvy@gmail.com>
2025-01-01 07:51:59 +0900

    [Refactor] 로그인 페이지 리팩토링
    로그인 페이지에 사용되는 인풋 필드와 버튼들 컴포넌트화 했음

--- src/components/features/Sign/SubmitButton.tsx
+++ src/components/features/Sign/SubmitButton.tsx
@@ -1,8 +1,14 @@
-const SubmitButton = ({ children }: { children: React.ReactNode }) => {
+import React from 'react';
+
+interface SubmitButtonProps {
+    children: React.ReactNode;
+}
+
+const SubmitButton: React.FC<SubmitButtonProps> = ({ children }) => {
     return (
         <button
             type="submit"
-            className="w-full py-3 bg-blue-500 rounded text-white font-semibold hover:bg-blue-600"
+            className="w-full bg-[#246BFD] text-white py-3 rounded-full text-[16px] font-semibold text-lg hover:bg-blue-600 transition-all"
         >
             {children}
         </button>
