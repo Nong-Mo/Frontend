afce58a0b8cc5a931fbc45c95fd4b6f6ebe9e380
hanvy072 <072hanvy@gmail.com>
2024-12-31 19:13:16 +0900

    [Style] 회원가입 버튼에 노출되는 텍스트 수정

--- src/pages/SignUp.tsx
+++ src/pages/SignUp.tsx
@@ -1,40 +1,43 @@
 import React, { useState } from 'react';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
-import FormContainer from '../components/features/SignUp/FormContainer';
-import InputField from '../components/features/SignUp/InputField';
-import SubmitButton from '../components/features/SignUp/SubmitButton';
+import { FaEye, FaEyeSlash } from 'react-icons/fa';
+import FormContainer from '../components/features/Sign/FormContainer';
+import InputField from '../components/features/Sign/InputField';
+import SubmitButton from '../components/features/Sign/SubmitButton';
 
 const Signup = () => {
     const [formData, setFormData] = useState({
         nickname: '',
         email: '',
         password: '',
-        confirmPassword: '',
+        password_confirmation: '',
     });
 
     const [errors, setErrors] = useState({
         nickname: '',
         email: '',
         password: '',
-        confirmPassword: '',
+        password_confirmation: '',
     });
 
-    const [loading, setLoading] = useState(false);
+    const [showPassword, setShowPassword] = useState(false);
+    const [showPasswordConfirmation, setShowPasswordConfirmation] = useState(false);
+
     const navigate = useNavigate();
 
     const isValidPassword = (password) => {
         const lengthCheck = password.length >= 8 && password.length <= 20;
         const spaceCheck = !/\s/.test(password);
         const types = [
-            /[A-Z]/.test(password),
-            /[a-z]/.test(password),
-            /[0-9]/.test(password),
-            /[!@#$%^&*(),.?":{}|<>]/.test(password),
+            /[A-Z]/.test(password), // 대문자
+            /[a-z]/.test(password), // 소문자
+            /[0-9]/.test(password), // 숫자
+            /[!@#$%^&*(),.?":{}|<>]/.test(password), // 특수문자
         ];
         const typeCount = types.filter(Boolean).length;
 
-        return lengthCheck && spaceCheck && typeCount >= 2;
+        return lengthCheck && spaceCheck && typeCount >= 3; // 최소 3종류 포함
     };
 
     const handleChange = (e) => {
@@ -42,10 +45,13 @@ const Signup = () => {
         setFormData((prevData) => ({ ...prevData, [name]: value }));
 
         let error = '';
+        if (name === 'email' && !/^[\w-]+(\.[\w-]+)*@([\w-]+\.)+[a-zA-Z]{2,7}$/.test(value)) {
+            error = '유효한 이메일 주소를 입력해주세요.';
+        }
         if (name === 'password' && !isValidPassword(value)) {
             error = '비밀번호는 8~20자 사이여야 하며, 공백 없이 두 종류 이상의 문자를 포함해야 합니다.';
         }
-        if (name === 'confirmPassword' && value !== formData.password) {
+        if (name === 'password_confirmation' && value !== formData.password) {
             error = '비밀번호와 비밀번호 확인이 일치하지 않습니다.';
         }
 
@@ -54,8 +60,12 @@ const Signup = () => {
 
     const handleSubmit = async (e) => {
         e.preventDefault();
-        const { nickname, email, password, confirmPassword } = formData;
+        const { nickname, email, password, password_confirmation } = formData;
         const newErrors = {};
+
+        if (nickname.length < 3) {
+            newErrors.nickname = '닉네임은 3자 이상이어야 합니다.';
+        }
 
         if (!/^[\w-]+(\.[\w-]+)*@([\w-]+\.)+[a-zA-Z]{2,7}$/.test(email)) {
             newErrors.email = '유효한 이메일 주소를 입력해주세요.';
@@ -66,20 +76,19 @@ const Signup = () => {
                 '비밀번호는 8~20자 사이여야 하며, 공백 없이 두 종류 이상의 문자를 포함해야 합니다.';
         }
 
-        if (password !== confirmPassword) {
-            newErrors.confirmPassword = '비밀번호와 비밀번호 확인이 일치하지 않습니다.';
+        if (password !== password_confirmation) {
+            newErrors.password_confirmation = '비밀번호와 비밀번호 확인이 일치하지 않습니다.';
         }
 
         setErrors(newErrors);
 
         if (Object.keys(newErrors).length === 0) {
-            setLoading(true);
             try {
-                console.log(formData)
-                const response = await axios.post('/auth/signup', {
-                    username: nickname,
-                    email,
-                    password,
+                const response = await axios.post('https://e6be-118-34-210-78.ngrok-free.app/auth/signup', {
+                    email: email,
+                    nickname: nickname,
+                    password: password,
+                    password_confirmation: password_confirmation,
                 });
                 if (response.status === 201) {
                     alert('회원가입에 성공했습니다.');
@@ -90,8 +99,6 @@ const Signup = () => {
                     ...prevErrors,
                     apiError: '회원가입에 실패했습니다.',
                 }));
-            } finally {
-                setLoading(false);
             }
         }
     };
@@ -114,7 +121,6 @@ const Signup = () => {
                         onChange={handleChange}
                         placeholder="닉네임을 입력하세요."
                         error={errors.nickname}
-                        disabled={loading}
                     />
                     <InputField
                         label="이메일"
@@ -124,31 +130,50 @@ const Signup = () => {
                         onChange={handleChange}
                         placeholder="example@domain.com"
                         error={errors.email}
-                        disabled={loading}
                     />
-                    <InputField
-                        label="비밀번호"
-                        type="password"
-                        name="password"
-                        value={formData.password}
-                        onChange={handleChange}
-                        placeholder="8~20자 비밀번호를 입력하세요."
-                        error={errors.password}
-                        disabled={loading}
-                    />
-                    <InputField
-                        label="비밀번호 확인"
-                        type="password"
-                        name="confirmPassword"
-                        value={formData.confirmPassword}
-                        onChange={handleChange}
-                        placeholder="비밀번호를 한 번 더 입력하세요."
-                        error={errors.confirmPassword}
-                        disabled={loading}
-                    />
+                    <div className="relative">
+                        <InputField
+                            label="비밀번호"
+                            type={showPassword ? "text" : "password"}
+                            name="password"
+                            value={formData.password}
+                            onChange={handleChange}
+                            placeholder="비밀번호를 입력하세요."
+                            error={errors.password}
+                        />
+                        <div
+                            onClick={() => setShowPassword(!showPassword)}
+                            className="absolute inset-y-0 right-0 pr-3 flex items-center text-sm leading-5 cursor-pointer"
+                        >
+                            {showPassword ? <FaEyeSlash className="text-gray-500" size={20}/> :
+                                <FaEye className="text-gray-500" size={20}/>}
+                        </div>
+                    </div>
+
+                    <div className="relative">
+                        <InputField
+                            label="비밀번호 확인"
+                            type={showPasswordConfirmation ? "text" : "password"}
+                            name="password_confirmation"
+                            value={formData.password_confirmation}
+                            onChange={handleChange}
+                            placeholder="비밀번호를 한 번 더 입력하세요."
+                            error={errors.password_confirmation}
+
+                        />
+                        <div
+                            onClick={() => setShowPasswordConfirmation(!showPasswordConfirmation)}
+                            className="absolute inset-y-0 right-0 pr-3 flex items-center text-sm leading-5 cursor-pointer"
+                        >
+                            {showPasswordConfirmation ? <FaEyeSlash className="text-gray-500" size={20}/> :
+                                <FaEye className="text-gray-500" size={20}/>}
+                        </div>
+                    </div>
+
                     {errors.apiError && <p className="text-red-500 text-xs mt-1">{errors.apiError}</p>}
-                    <SubmitButton className="w-full bg-blue-500 text-white py-2 rounded hover:bg-blue-600" disabled={loading}>
-                        {loading ? '회원가입 중...' : '회원가입'}
+
+                    <SubmitButton className="w-full bg-blue-500 text-white py-2 rounded hover:bg-blue-600">
+                        회원가입
                     </SubmitButton>
                 </form>
             </FormContainer>
