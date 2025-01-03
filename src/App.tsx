0b0ba8199ee0c7c7b12182babe1607d31a06b698
rbase0218 <rbase@naver.com>
2025-01-03 21:43:13 +0900

    [Fix] Route 상수화 / Private Page 적용

--- src/App.tsx
+++ src/App.tsx
@@ -7,37 +7,47 @@ import {
 import SignIn from "./pages/SignIn";
 import SignUp from "./pages/SignUp";
 import Scan from "./pages/Scan";
-import LibraryViewer from "./pages/LibraryViewer.tsx";
-import Player from "./pages/Player.tsx";
+import LibraryViewer from "./pages/LibraryViewer";
+import Player from "./pages/Player";
 import Home from "./pages/Home";
-import Intro from "./pages/Intro.tsx";
-import PrivateRoute from "./components/common/PrivateRoute.tsx";
+import Intro from "./pages/Intro";
+import PrivateRoute from "./components/common/PrivateRoute";
+import MainLayout from "./components/common/MainLayout";
+import { ROUTES } from "./routes/constants";
+import {useEffect} from "react";
 
 const App = () => {
+  useEffect(() => {
+    const vh = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty('--vh', `${vh}px`);
+
+    window.addEventListener('resize', () => {
+      const vh = window.innerHeight * 0.01;
+      document.documentElement.style.setProperty('--vh', `${vh}px`);
+    });
+  }, []);
   return (
-    <div className=" main-wrapper flex justify-center item-center min-h-screen">
       <Router>
-        <main className="content-container flex justify-center w-440 h-[956px] relative">
-          <Routes>
-            <Route path="/intro" element={<Intro/>} />
-            <Route 
-              path="/home" 
-              element={
-                <PrivateRoute>
-                  <Home />
-                </PrivateRoute>
-              } 
-            />
-            <Route path="/signin" element={<SignIn />} />
-            <Route path="/scan" element={<Scan />} />
-            <Route path="/signup" element={<SignUp />} />
-            <Route path="/library" element={<LibraryViewer />} />
-            <Route path="/player" element={<Player />} />
-            <Route path="/" element={<Navigate to="/intro" replace />} />
-          </Routes>
-        </main>
+        <Routes>
+          <Route element={<MainLayout />}>
+            {/* Public Routes */}
+            <Route path={ROUTES.INTRO.path} element={<Intro />} />
+            <Route path={ROUTES.SIGN_IN.path} element={<SignIn />} />
+            <Route path={ROUTES.SIGN_UP.path} element={<SignUp />} />
+
+            {/* Protected Routes */}
+            <Route element={<PrivateRoute />}>
+              <Route path={ROUTES.HOME.path} element={<Home />} />
+              <Route path={ROUTES.SCAN.path} element={<Scan />} />
+              <Route path={ROUTES.LIBRARY.path} element={<LibraryViewer />} />
+              <Route path={ROUTES.PLAYER.path} element={<Player />} />
+            </Route>
+
+            {/* Redirect */}
+            <Route path="/" element={<Navigate to={ROUTES.INTRO.path} replace />} />
+          </Route>
+        </Routes>
       </Router>
-    </div>
   );
 };
 
