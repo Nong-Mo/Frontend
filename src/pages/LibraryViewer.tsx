83c70019c6b6f2674fafbb2ff2519eea620d7c14
rbase0218 <rbase@naver.com>
2025-01-04 20:28:42 +0900

    [Fix] LibraryViewer에서 사용되는 컴포넌트 추가

--- src/pages/LibraryViewer.tsx
+++ src/pages/LibraryViewer.tsx
@@ -1,35 +1,12 @@
 import React, { useState } from "react";
 import { NavBar } from "../components/common/NavBar.tsx";
-import ViewerContent from "../components/LibraryViewer/ViewerContent.tsx";
 
 const LibraryViewer = () => {
-  const [emptyTab, setEmptyTab] = useState(true);
-
-  // Logic
-  // 1. 접속 했을 경우, 유저의 보관함에 책이 있는지 확인한다.
-  // (있다) -> emptyTab = false
-  // (없다) -> emptyTab = true
-  return (
-    <div className="page-container flex flex-col z-10">
-      {/*  상단 네비게이션 바 */}
-      <NavBar title={"책 보관함"} rightIcon="search"/>
-
-      {/*  텍스트 영역 */}
-      <div className="flex flex-col pl-[41px] pt-[4px]">
-        <div className="primary-info-text">
-          {emptyTab ? "보관함이 비었어요" : "감상하고 싶은"}
+    return (
+        <div>
+            <h1>Library Viewer Page</h1>
         </div>
-        <div className="primary-info-text mt-[-5px]">
-          {emptyTab ? "책을 추가해 주세요" : "책을 선택해 주세요"}
-        </div>
-      </div>
-
-      {/*  Content 영역 */}
-      <div className="flex justify-center">
-        <ViewerContent width={327} height={713} />
-      </div>
-    </div>
-  );
+    );
 };
 
 export default LibraryViewer;
