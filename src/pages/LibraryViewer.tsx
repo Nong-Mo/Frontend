a9bae65b8c5ba170486d0adb781f54035f7469c3
rbase0218 <rbase@naver.com>
2025-01-05 17:52:57 +0900

    [Fix] Viewer 컴포넌트 레이아웃 배치 및 컴포넌트 분리 완료

--- src/pages/LibraryViewer.tsx
+++ src/pages/LibraryViewer.tsx
@@ -1,10 +1,56 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { NavBar } from "../components/common/NavBar.tsx";
+import CollectionGrid from "../components/viewer/CollectionGrid";
+import { FaPlus } from "react-icons/fa";
 
 const LibraryViewer = () => {
+    const [filterButton, setFilterButton] = useState(0);
+
+    const onClickAllButton = () => {
+        setFilterButton(0);
+    }
+
+    const onClickRecentButton = () => {
+        setFilterButton(1);
+    }
+
     return (
-        <div>
-            <h1>Library Viewer Page</h1>
+        <div className="w-full z-10">
+            <div className="content-wrapper ml-[32px] mr-[32px] mt-[15px] md-[34px] w-[350px] flex flex-col items-center h-[896px]">
+                <div className="w-full">
+                    <NavBar title="책 보관함"
+                            rightIcon="search"/>
+                </div>
+                <h1 className="primary-info-text leading-50">
+                    감상하고 싶은<br/>책을 선택해 주세요.
+                </h1>
+                <div className="w-full h-[67.2px] mt-[30px]">
+                    <button className="w-full h-full flex items-center justify-center rounded-[16.5px] bg-[#262A34]">
+                        <FaPlus className="w-[15.14px] h-[16.8px] text-white"/>
+                    </button>
+                </div>
+                <div className="flex justify-center h-8 mt-[30px]">
+                    <button
+                        onClick={onClickAllButton}
+                        className={`w-[93px] h-full text-white mr-[5px] ${
+                            filterButton === 0 ? 'bg-[#246BFD] rounded-[25px]' : ''
+                        }`}
+                    >
+                        전체 목록
+                    </button>
+                    <button
+                        onClick={onClickRecentButton}
+                        className={`w-[93px] h-full text-white ml-[5px] ${
+                            filterButton === 1 ? 'bg-[#246BFD] rounded-[25px]' : ''
+                        }`}
+                    >
+                        최근 읽은 책
+                    </button>
+                </div>
+                <div className="h-[550px] overflow-y-auto w-full mt-[30px] [&::-webkit-scrollbar]:hidden">
+                    <CollectionGrid/>
+                </div>
+            </div>
         </div>
     );
 };
