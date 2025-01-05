2c426e9b4cd4f9e66beda0f2b01114083af975f5
seoha <a1vm5h@gmail.com>
2025-01-05 20:17:03 +0900

    [Fix] signin, signup UI 수정

--- src/pages/SignUp.tsx
+++ src/pages/SignUp.tsx
@@ -80,10 +80,10 @@ const SignUpPage: React.FC = () => {
   };
 
   return (
-      <div className="flex flex-col items-center px-[32px] z-10">
+      <div className="w-full h-[896px] flex flex-col px-[32px] z-10">
 
         {/* NavBar 영역 */}
-        <NavBar title="회원가입" />
+        <NavBar title="회원가입" showMenu={false} />
 
         {/* Contents 영역 */}
         <div className="w-[350px] h-[729px] flex flex-col items-center">
