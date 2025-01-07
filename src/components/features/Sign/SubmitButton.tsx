393e8c32c6ba8469e251b0ef38d4019940a00895
hanvy072 <072hanvy@gmail.com>
2025-01-07 13:04:06 +0900

    [Feat] Intro 페이지 레이아웃 작업 완료

--- src/components/features/Sign/SubmitButton.tsx
+++ src/components/features/Sign/SubmitButton.tsx
@@ -3,12 +3,13 @@ import React from 'react';
 interface SubmitButtonProps {
     onClick: (e: React.MouseEvent<HTMLButtonElement>) => void;
     children: React.ReactNode;
+    className?: string;
 }
 
-const SubmitButton: React.FC<SubmitButtonProps> = ({ onClick, children }) => {
+const SubmitButton: React.FC<SubmitButtonProps> = ({ onClick, children, className }) => {
     return (
         <button
-            className="w-[350px] h-[55px] bg-[#246bfd] rounded-[25px] justify-center items-center gap-0.5 inline-flex"
+            className={`w-[350px] h-[55px] bg-[#246bfd] rounded-[25px] justify-center items-center gap-0.5 inline-flex ${className}`}
             onClick={onClick}
         >
             <div className="big-button-text">
