5aa12d9327c97e6c4045fd20e114e5c7ec53e6b2
rbase0218 <rbase@naver.com>
2025-01-08 05:40:44 +0900

    [Fix] CollectionItemProps 인터페이스 Export

--- src/components/viewer/CollectionGrid.tsx
+++ src/components/viewer/CollectionGrid.tsx
@@ -1,20 +1,38 @@
 import React from 'react';
 import CollectionItem from "./CollectionItem.tsx";
 
+export interface CollectionItemProps {
+    fileID: string;
+    fileName: string;
+    uploadDate: string;
+}
+
 interface CollectionGridProps {
-    items : any[];
+    items : CollectionItemProps[];
 }
 
 const CollectionGrid = ({items} : CollectionGridProps) => {
+
+    const formatDate = (dateString: string) => {
+        const date = new Date(dateString);
+        const year = date.getFullYear();
+        const month = String(date.getMonth() + 1).padStart(2, '0');
+        const day = String(date.getDate()).padStart(2, '0');
+        const hours = String(date.getHours()).padStart(2, '0');
+        const minutes = String(date.getMinutes()).padStart(2, '0');
+
+        return `${year}-${month}-${day} ${hours}:${minutes}`;
+    };
+
     return (
         <div className="w-full">
             <div className="h-full">
                 <div className="grid grid-cols-2 gap-[15px] pb-[30px]">
                     {items?.map(item => (
                         <CollectionItem
-                            key={item.id}
-                            title={item.title}
-                            date={item.date}
+                            key={item.fileID}
+                            title={item.fileName}
+                            date={formatDate(item.uploadDate)} // 포맷팅된 날짜 전달
                             itemType="BOOK"
                         />
                     ))}
