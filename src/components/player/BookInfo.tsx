f2d4c1bad58c12538e02ba92f2d9046586b45089
rbase0218 <rbase@naver.com>
2025-01-11 18:10:43 +0900

    [Fix] 타입 스크립트 엄격한 규율 적용

--- src/components/player/BookInfo.tsx
+++ src/components/player/BookInfo.tsx
@@ -4,7 +4,7 @@ interface BookInfoProps {
   bookName: string;
 }
 
-export const BookInfo: React.FC<BookInfoProps> = ({ bookName }) => {
+export const BookInfo: React.FC<BookInfoProps> = ({ bookName } : BookInfoProps) => {
   return (
     <div className="flex flex-col w-full gap-2 mb-8 items-center text-center">
       <div className="text-white text-xl font-semibold">
