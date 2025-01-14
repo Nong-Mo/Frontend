5a0f44489360dd42e3238822ddd906775152798f
rbase0218 <rbase@naver.com>
2025-01-14 13:14:27 +0900

    [Feat] Home에서 활성화되지 않은 버튼은 Gray 처리

--- src/components/features/Home/BoxList.tsx
+++ src/components/features/Home/BoxList.tsx
@@ -6,38 +6,47 @@ interface BoxListProps {
     count: number;
     icon: React.ReactNode;
     countColor?: string;
+    isActive: boolean;
 }
 
-const BoxList: React.FC<BoxListProps> = ({ title, count, icon, countColor }) => {
+const BoxList: React.FC<BoxListProps> = ({ title, count, icon, countColor, isActive }) => {
+    // 기본 회색 색상
+    const grayColor = '#808080';
+
     const iconWithColor = React.cloneElement(icon as React.ReactElement, {
-        style: { color: '#200745', fontSize: '20px' },
+        style: {
+            color: '#200745',
+            fontSize: '20px'
+        },
     });
 
     return (
-        <div className="hover:border-gradient relative w-[350px] h-[67.2px] mt-[15px] rounded-2xl">
-            {/* 내부 컨텐츠 */}
+        <div className={`hover:border-gradient relative w-[350px] h-[67.2px] mt-[15px] rounded-2xl ${!isActive && 'opacity-50'}`}>
             <div className="content flex items-center w-full h-full">
-                {/* 아이콘 */}
                 <div
                     className="w-[50.4px] h-[50.4px] flex-shrink-0 overflow-hidden flex items-center justify-center rounded-[12.6px]"
-                    style={{backgroundColor: countColor}}
+                    style={{
+                        backgroundColor: isActive ? countColor : grayColor
+                    }}
                 >
                     {iconWithColor}
                 </div>
 
-                {/* 제목과 숫자 */}
                 <div className="flex-1 ml-4">
-                    <p className="text-[17.5px] font-bold">{title}</p>
+                    <p className={`text-[17.5px] font-bold ${!isActive && 'text-gray-500'}`}>
+                        {title}
+                    </p>
                 </div>
                 <span
                     className="text-[17.5px] font-bold"
-                    style={{color: countColor}}
+                    style={{
+                        color: isActive ? countColor : grayColor
+                    }}
                 >
-            {count}
-        </span>
+                    {count}
+                </span>
 
-                {/* 화살표 아이콘 */}
-                <FaGreaterThan className="ml-4 w-3 h-3 text-white"/>
+                <FaGreaterThan className={`ml-4 w-3 h-3 ${isActive ? 'text-white' : 'text-gray-500'}`}/>
             </div>
         </div>
     );
