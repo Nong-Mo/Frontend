a37a133b1005af1a827215d95488dd7c5a9c430a
rbase0218 <rbase@naver.com>
2025-01-15 03:57:15 +0900

    [Fix] 중복 태그 제거 및 ServiceWorker 설정

--- src/service-worker.ts
+++ src/service-worker.ts
@@ -60,3 +60,11 @@ self.addEventListener('activate', event => {
     );
     return self.clients.claim();
 });
+
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        fetch(event.request).then((response) => {
+            return response;
+        })
+    );
+});
