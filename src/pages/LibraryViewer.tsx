4333c2873d4b76d972d29e90a24e0d99ebf460c5
rbase0218 <rbase@naver.com>
2025-01-14 21:23:36 +0900

    [Feat] UseEffect 의존성 제거로 접근 할 때 마다 API 호출

--- src/pages/LibraryViewer.tsx
+++ src/pages/LibraryViewer.tsx
@@ -54,7 +54,7 @@ const LibraryViewer = ({collectionType} : LibraryViewerProps) => {
         };
 
         fetchCollectionItems();
-    }, [collectionType]);
+    }, []);
 
     const onClickAllButton = () => {
         setFilterButton(0);
