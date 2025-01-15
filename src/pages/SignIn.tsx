5a0ce14a3cbc68e8e416a667bf0bd9627a70248f
rbase0218 <rbase@naver.com>
2025-01-16 01:04:45 +0900

    [Chore] 의존성 추가

--- src/pages/SignIn.tsx
+++ src/pages/SignIn.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, {useEffect, useState} from "react";
 import { Link } from "react-router-dom";
 import InputField from "../components/features/Sign/InputField";
 import ErrorMessage from "../components/features/Sign/ErrorMessage";
